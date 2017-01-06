a72c645b8a61955dddf7c93efa755132799e747f
jbub <contact@jbub.eu>
2017-01-06 22:15:41 +0100

    Use less aggresive compression of css to prevent merge conflicts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const perfectionist = require('perfectionist');
+const discardComments = require('postcss-discard-comments');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,17 +16,23 @@ module.exports = {
     filename: '[name].css'
   },
   plugins: [
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin('[name].css'),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: discardComments
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: perfectionist,
+      cssProcessorOptions: {
+        format: 'compact'
+      }
+    })
   ],
   module: {
     loaders: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?minimize!sass-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
       }
     ]
-  },
-  sassLoader: {
-    outputStyle: 'compressed'
   }
 };
